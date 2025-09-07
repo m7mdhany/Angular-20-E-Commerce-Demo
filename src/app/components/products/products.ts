06ea8ad6e65ea5d8f441998e2ea52a3692aefdb3
m7mdhany <muhamedhany.m@gmail.com>
2025-09-07 08:51:42 +0300

    update:products page

--- src/app/components/products/products.ts
+++ src/app/components/products/products.ts
@@ -25,50 +25,62 @@ export class Products implements OnInit {
   totalPages: number = 0;
   paginatedProducts: Iproducts[] = [];
 
+  private searchTimeout: any; // للـ debounce
+
   ngOnInit(): void {
     this.getProducts();
+  }
+
+  getProducts() {
     this.loading = true;
     this.error = null;
-  }
-  getProducts() {
     this.productServices.getProducts().subscribe({
       next: (data) => {
         this.products = data.products;
         this.filteredProducts = [...this.products];
         this.extractCategories();
-        this.loading = false;
         this.totalPages = Math.ceil(this.filteredProducts.length / this.itemsPerPage);
         this.updatePaginatedProducts();
+        this.loading = false;
       },
-      error: (error) => console.log('Error:', error),
+      error: (error) => {
+        this.error = 'Failed to load products. Please try again later.';
+        this.loading = false;
+      }
     });
   }
+
   extractCategories() {
     const cats = this.products.map(p => p.category);
     this.categories = Array.from(new Set(cats));
   }
+
   filterProducts() {
-    this.filteredProducts = this.products.filter(p => {
-      const matchesCategory = this.selectedCategory ? p.category === this.selectedCategory : true;
-      const matchesSearch = p.title.toLowerCase().includes(this.searchText.toLowerCase());
-      console.log(matchesCategory);
-      console.log(matchesSearch);
-      return matchesCategory && matchesSearch;
-    });
-    this.totalPages = Math.ceil(this.filteredProducts.length / this.itemsPerPage);
-    this.currentPage = 1;
-    this.updatePaginatedProducts();
+    clearTimeout(this.searchTimeout);
+    this.searchTimeout = setTimeout(() => {
+      this.filteredProducts = this.products.filter(p => {
+        const matchesCategory = this.selectedCategory ? p.category === this.selectedCategory : true;
+        const matchesSearch = p.title.toLowerCase().includes(this.searchText.toLowerCase());
+        return matchesCategory && matchesSearch;
+      });
+      this.totalPages = Math.ceil(this.filteredProducts.length / this.itemsPerPage);
+      this.currentPage = 1;
+      this.updatePaginatedProducts();
+    }, 400); // 0.4 ثانية
   }
+
   updatePaginatedProducts() {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = startIndex + this.itemsPerPage;
     this.paginatedProducts = this.filteredProducts.slice(startIndex, endIndex);
   }
+
   changePage(page: number) {
     if (page < 1 || page > this.totalPages) return;
     this.currentPage = page;
     this.updatePaginatedProducts();
   }
+
   get pagesArray(): number[] {
     return Array(this.totalPages).fill(0).map((_, i) => i + 1);
   }
